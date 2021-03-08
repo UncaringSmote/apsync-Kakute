bd4aab3be9f08e9cd3722d2fe8d00696fa3eab31
UncaringSmote <pnut008@aol.com>
2021-03-08 18:49:33 -0500

    Update main2.sh

--- main2.sh
+++ main2.sh
@@ -16,10 +16,10 @@ echo "PATH=\$PATH:~/.local/bin" >> ~/.profile
 source ~/.profile
 
 ## Add required packages for general
-sudo apt install git screen python3 python3-dev python3-pip python3-matplotlib -y
-sudo apt install -y libxml2-dev libxslt1.1 libxslt1-dev libz-dev
+sudo apt install git screen python python-dev python-pip python-matplotlib -y
+sudo apt install -y libxml2-dev libxslt1.1 libxslt1-dev libz-dev python-lxml
 #sudo pip3 install --upgrade pip
-pip3 install pyserial future pymavlink mavproxy --user
+pip install pyserial future pymavlink mavproxy --user
 
 ## Install a package that will automatically mount & unmount USB drives
 sudo apt install usbmount -y
